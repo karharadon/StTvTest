81dc68bd00f612a0ff70a3745fe14458e1d61daa
Lastochkin <dmytruk.andrii@gmail.com>
2017-01-08 03:32:47 +0200

    add chromedriver

--- src/test/java/lastochkin/streamTV/helpers/GuiceTestModule.java
+++ src/test/java/lastochkin/streamTV/helpers/GuiceTestModule.java
@@ -34,6 +34,7 @@ public class GuiceTestModule implements Module {
             return driver;}
         else {
             if (browser.equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver");
                 driver = new ChromeDriver();
             }
             if (browser.equals("firefox")) {
